3153abebf9213c81d31c844ec1e7da5dfec96ffb
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-02-11 11:39:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.10%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        store = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in store:
+                return [store[complement], i]
+            store[num] = i
+        return []
+
+        return res
+
+        
