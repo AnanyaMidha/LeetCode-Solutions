de0a41781c6944a850ccb5a047e4a8a2ee69b626
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-02-24 08:37:18 +0530

    Time: 147 ms (10.95%) | Memory: 19.5 MB (11.46%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        mini = prices[0]
+        res = 0
+
+        for i in range(1, len(prices)):
+            mini = min(mini, prices[i])
+            res = max(res, prices[i] - mini)
+
+        return res
+
+           
