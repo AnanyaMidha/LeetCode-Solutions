aa41f0a0c859f57da6d2c1998341b8aa996dcb2a
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-03-18 11:25:59 +0530

    Time: 147 ms (10.78%) | Memory: 19.3 MB (36.75%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,12 +5,13 @@ class Solution(object):
         :rtype: int
         """
         mini = prices[0]
+
         res = 0
 
         for i in range(1, len(prices)):
             mini = min(mini, prices[i])
-            res = max(res, prices[i] - mini)
 
+            res = max(res, prices[i] - mini)
         return res
 
            
