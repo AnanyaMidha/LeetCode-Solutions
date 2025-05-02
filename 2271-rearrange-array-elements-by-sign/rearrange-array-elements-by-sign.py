d2d254eeb46e048be3b896e0b8e60b7c6aa92bd3
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-05-02 23:41:47 +0530

    Time: 102 ms (53.54%) | Memory: 40.8 MB (98.79%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        pos, neg = [], []
+
+        for n in nums:
+            if n > 0:
+                pos.append(n)
+            else:
+                neg.append(n)
+        
+        i = 0
+
+        while (2*i) < len(nums):
+            nums[2*i] = pos[i]
+            nums[2*i + 1] = neg[i]
+            i += 1
+        return nums
+                
