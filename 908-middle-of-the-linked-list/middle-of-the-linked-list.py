6c3f15cae7c76603347350bc93c006c426b249d0
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-05-29 23:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.14%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -10,7 +10,6 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         slow, fast = head, head
-
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
