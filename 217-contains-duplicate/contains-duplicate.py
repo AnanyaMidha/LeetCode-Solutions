60733ca1b73ca6e3163e67734dcec306a51b4dcd
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-12-26 22:13:00 +0530

    Time: 16 ms (43.68%) | Memory: 29.1 MB (85.04%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,17 +1,6 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-
-        count = {}
-
-        for i in nums:
-            # Initialize the count for the element if it doesn't exist
-            if i not in count:
-                count[i] = 0
-            count[i] += 1
-            if count[i] > 1:
-                return True
-        return False
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(set(nums)) != len(nums):
+            return True
+        else:
+            return False
