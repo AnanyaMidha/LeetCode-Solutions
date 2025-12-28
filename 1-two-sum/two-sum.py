6ca32dcfe7f50ff6c8e78961863f616ab3e6b33f
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2025-12-28 21:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (68.40%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,6 +5,6 @@ class Solution:
         for i, num in enumerate(nums):
             complement = target - num
             if complement in store:
-                return [store[complement], i]
+                return [i, store[complement]]
             store[num] = i
         return
