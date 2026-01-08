18ac4bdb495432e08fa4939e7eccab6810f56246
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2026-01-08 22:40:11 +0530

    Time: 3 ms (37.35%) | Memory: 19.3 MB (10.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,23 +1,15 @@
-class Solution(object):
-   def isValid(self, s):
-    """
-    :type s: str
-    :rtype: bool
-    """
-    st = []
-    mapping = {')': '(', ']': '[', '}': '{'}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        mapping = {')': '(', ']': '[', '}': '{'}
 
-    for char in s:
-        if char in mapping.values():
-            st.append(char)
-        elif char in mapping:
-            if not st or st.pop() != mapping[char]:
-                return False
-        else:
-            return False  #
+        for char in s:
+            if char in mapping.values():
+                st.append(char)
+            elif char in mapping:
+                if not st or st.pop() != mapping[char]:
+                    return False
+            else:
+                return False  #
 
-    return not st
-
-
-                
-        
+        return not st
