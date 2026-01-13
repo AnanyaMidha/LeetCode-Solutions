1e30187f3ae3f0eadb87d7d3da423ee6ad490eb0
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2026-01-13 22:52:44 +0530

    Time: 7 ms (82.84%) | Memory: 19.4 MB (28.42%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         str1 = ""
 
         for i in s:
             if i.isalnum():
                 str1 += i.lower()
         return str1 == str1[::-1]
-        
-        
