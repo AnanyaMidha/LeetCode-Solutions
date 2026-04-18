5d6caa31cf27d7668818790c0448b66429bbbef1
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2026-04-19 01:23:38 +0530

    Time: 555 ms (79.62%) | Memory: 22.2 MB (83.62%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,34 +1,27 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
         nums.sort()
-        n = len(nums)
-        triplets = []
 
-        for i in range(n - 2):
-            if nums[i] > 0:
-                break
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
                 continue
-            left, right = i + 1, n - 1
-
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-
-                if current_sum < 0:
-                    left += 1
-                elif current_sum > 0:
-                    right -= 1
+            l, r = i+1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r-=1
+                elif threeSum < 0:
+                    l += 1 
                 else:
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    left, right = left + 1, right - 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-        return triplets
-                
-        
+                    res.append([a, nums[l], nums[r]])
+
+
+                    l +=1
+
+                    while nums[l]  == nums[l - 1] and l < r:
+                        l += 1
+        return res
+
+
+            
