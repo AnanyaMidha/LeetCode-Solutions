ebb66c6148c23e93b3807532be0cf76d31dec7e6
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2026-04-27 00:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (77.18%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,36 +1,17 @@
-class Solution(object):
-    def recursive(self, nums, low, high, target):
-        if low > high:
-            return - 1
-        mid = (low + high)//2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            return self.recursive(nums, low, mid - 1, target)
-        else:
-            return self.recursive(nums, mid + 1, high, target)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
 
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # left, right = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
 
-        # while left <= right:
-        #     mid = (left + right)//2
-        #     if(nums[mid] == target):
-        #         return mid
-        #     elif(nums[mid] < target):
-        #         left = mid + 1
-        #     elif(nums[mid] > target):
-        #         right = mid - 1
-        # return -1
-        # Recursive code:
-        return self.recursive(nums, 0, len(nums) - 1, target)
+            if nums[mid] == target:
+                return mid
 
+            elif nums[mid] < target:
+                low = mid + 1
 
-            
-            
-        
+            else:
+                high = mid - 1
+        return -1
+                
