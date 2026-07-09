8f400f4ea14831b4de9f4e5881053cf2089d64bc
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2026-07-09 22:35:59 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.20%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        lastNonZero = 0 # this will move to the next non zero elemnet
+        LNZ = 0 #this will point to the next non zero element
 
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[lastNonZero] = nums[lastNonZero], nums[i]
-                lastNonZero += 1
-        
+                nums[i], nums[LNZ] = nums[LNZ], nums[i]
+                LNZ += 1
